663ee475b671f1b07097bc8924d6b4c8b43e6e2d
Rahul Goswami <rahul.goswami@mintifi.com>
2025-10-17 14:39:10 +0530

    button chnage but needs to replace again may be

--- client/src/helpers/button/luckySpin.jsx
+++ client/src/helpers/button/luckySpin.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 const ButtonLuckySpin = ({ onClick }) => {
   return (
     <button
-      className='text-sm bg-[#fff] text-black border border-[#fff] rounded-md cursor-pointer px-12 py-2 shadow-[0_6px_0_#ccc] active:shadow-[0_2px_0_#ccc] active:relative active:top-[2px] transition-all duration-100 font-semibold'
+      className="relative h-[50px] px-[30px] border-2 border-white bg-black select-none whitespace-nowrap transition-all duration-100 font-inherit text-white hover:cursor-crosshair active:scale-95 font-semibold text-[15px] z-3"
       onClick={onClick}
     >
-      Draw Spin
+      <div className="absolute w-[calc(100%+6px)] h-[calc(100%-16px)] top-2 left-[-3px] bg-black transition-all duration-200 hover:h-[calc(100%-32px)] hover:top-4"></div>
+      
+      <div className="absolute w-[calc(100%-16px)] h-[calc(100%+6px)] top-[-3px] left-2 bg-black transition-all duration-200 hover:w-[calc(100%-32px)] hover:left-4"></div>
+      
+      <span className="relative z-10 font-semibold">
+        Draw Spin
+      </span>
     </button>
   );
 };
